c9e97870da1d9d42082464ecdfa2be7e8561e57c
aNd12326 <andersonventosilla0@gmail.com>
2022-04-28 23:52:23 -0500

    53:00

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Analytics from "./components/Analytics";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import NewSletter from "./components/NewSletter";
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Analytics />
+      <NewSletter />
     </div>
   );
 }
